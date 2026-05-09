715f6a935e5ccbc23f31c1e59219a6267e9af902
ohahe52-dot <ohahe52@gmail.com>
2026-05-10 01:02:04 +0700

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import os
 import uvicorn
 from api import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, access_log=False, log_level="info")
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        access_log=False,
+        log_level="info"
+    )
